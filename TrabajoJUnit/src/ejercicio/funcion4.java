6221b5b38922016f948a64ffa41f3f96e4585e31
darconoje <dcondeojeda@gmail.com>
2019-03-16 12:30:07 +0100

    resuelta funcion #4

--- TrabajoJUnit/src/ejercicio/funcion4.java
+++ TrabajoJUnit/src/ejercicio/funcion4.java
@@ -10,7 +10,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class funcion4 {
-	
+	//Resuelto
 	/*Ejemplos de funciones*/
 	static Operaciones o = null;
 	static int operacionesRealizadas = 0;
